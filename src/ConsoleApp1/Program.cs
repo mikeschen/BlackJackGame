eeaef2e6e57feebb26ea7a67780b50210ce374c9
Michael Schennum <mikeschenphoto@gmail.com>
2016-11-29 16:45:24 -0700

    Add project files.

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public enum Suit
+    {
+        Heart, Diamond, Spade, Club
+    }
+    public enum Face
+    {
+        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+    class Card
+    {
+        public Face Face { get; set; }
+        public Suit Suit { get; set; }
+        public int Value { get; set; }
+    }
+
+    class Deck
+    {
+        private List<Card> cards;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 13; j++)
+                {
+                    cards.Add(new Card() { Suit = (Suit)i, Face = (Face)j });
+                    if (j <= 8)
+                    {
+                        cards[cards.Count - 1].Value = j + 1;
+                    } else
+                    {
+                        cards[cards.Count - 1].Value = 10;
+                    }
+                }
+            }
+        }
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                Deal();
+                Console.ReadLine();
+            }
+            public static void Deal()
+            {
+                int total = 0;
+                Deck myDeck = new Deck();
+                var rnd = new Random();
+                var result = rnd.Next(52);
+                Console.WriteLine(myDeck.cards[result].Face + " of " + myDeck.cards[result].Suit);
+                total = myDeck.cards[result].Value;
+                result = rnd.Next(52);
+                total += myDeck.cards[result].Value;
+                Console.WriteLine(myDeck.cards[result].Face + " of " + myDeck.cards[result].Suit);
+                Console.WriteLine("hit or stand?");
+                var response = Console.ReadLine();
+                if (response == "hit")
+                {
+                    Hit(total);
+                }
+                else if (response == "stand")
+                {
+                    Console.WriteLine("Player One Stands");
+                    Console.WriteLine(total);
+                }
+            }
+            public static void Hit(int total)
+            {
+                    var rnd = new Random();
+                    var result = rnd.Next(52);
+                    Deck myDeck = new Deck();
+                    total += myDeck.cards[result].Value;
+                    Console.WriteLine(myDeck.cards[result].Face + " of " + myDeck.cards[result].Suit);
+                    if (total > 21)
+                    {
+                        Console.WriteLine(total + " Bust!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("hit or stand?");
+                        var response = Console.ReadLine();
+                        if (response == "hit")
+                    {
+                        Hit(total);
+                    }
+                    else if (response == "stand")
+                    {
+                        Console.WriteLine("Player One Stands");
+                        Console.WriteLine(total);
+                    }
+                }
+                }
+        }
+    }
+}
